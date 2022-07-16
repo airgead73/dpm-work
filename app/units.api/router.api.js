fbd562e7d6f90472796afa881f6714e3a3e0a154
Dan Moneypenny <catullus1973@gmail.com>
2022-07-16 04:42:23 -0700

    update

--- app/units.api/router.api.js
+++ app/units.api/router.api.js
@@ -1,8 +1,5 @@
 const { Router } = require('express');
 const apiRouter = Router();
-const { authorRouter } = require('./authors');
-const { bookRouter } = require('./books');
-const { photoRouter } = require('./photos');
 const { shiftRouter } = require('./shifts');
 
 // middleware 
@@ -10,9 +7,6 @@ const { shiftRouter } = require('./shifts');
 //apiRouter.use(checkAuthClient);
 
 // routes
-apiRouter.use('/authors', authorRouter);
-apiRouter.use('/books', bookRouter);
-apiRouter.use('/photos', photoRouter);
 apiRouter.use('/shifts', shiftRouter);
 
 // export
--- app/units.client/router.client.js
+++ app/units.client/router.client.js
@@ -1,9 +1,6 @@
 const { Router } = require('express');
 const clientRouter = Router();
 const { homeRouter } = require('./home');
-const { authorRouter } = require('./authors');
-const { bookRouter } = require('./books');
-const { photoRouter } = require('./photos');
 const { shiftRouter } = require('./shifts');
 
 // middleware 
@@ -11,9 +8,6 @@ const { checkAuthClient } = require('../middleware');
 clientRouter.use(checkAuthClient);
 
 clientRouter.use('/', homeRouter);
-clientRouter.use('/authors', authorRouter);
-clientRouter.use('/books', bookRouter);
-clientRouter.use('/photos', photoRouter);
 clientRouter.use('/shifts', shiftRouter);
 
 // export
