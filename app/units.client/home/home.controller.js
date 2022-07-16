198a4dbc9d7db61f16afd851d65806d24772b987
Dan Moneypenny <catullus1973@gmail.com>
2022-07-16 02:44:44 -0700

    add shift folders

--- app/units.client/home/home.controller.js
+++ app/units.client/home/home.controller.js
@@ -12,7 +12,7 @@ exports.home = asyncHandler(async (req, res, next) => {
   return res
     .status(200)
     .render('pages/dashboard', {
-      title: 'starter',
+      title: 'work tracker',
       development: isDev
   });
 
---
+++ app/units.client/shifts/shifts.controller.js
@@ -0,0 +1,69 @@
+const asyncHandler = require('express-async-handler');
+const Shift = require('../../units.api/shifts/shift');
+const { isDev } = require('../../config/env');
+
+/**
+ * @desc shift dashboard view
+ * @route GET - /shifts
+ * @access Private
+ * */
+
+exports.dashboard = asyncHandler(async (req, res, next) => {
+
+  const { success, count, data: shifts } = res.results;
+
+  return res
+    .status(200)
+    .render('pages/shifts/dashboard', {
+      title: 'shifts',
+      success,
+      count,
+      shifts,
+      development: isDev
+    });
+});
+
+/**
+ * @desc shift add view
+ * @route GET - /shifts/add
+ * @access Private
+ * */
+
+ exports.add = asyncHandler(async (req, res, next) => {
+  return res
+    .status(200)
+    .render('pages/shifts/add', {
+      title: 'add shift',
+      development: isDev
+    });
+});
+
+/**
+ * @desc shift detail view
+ * @route GET - /shifts/:id
+ * @access Private
+ * */
+
+ exports.detail = asyncHandler(async (req, res, next) => {
+  return res
+    .status(200)
+    .render('pages/shifts/detail', {
+      title: 'shift detail',
+      development: isDev
+    });
+});
+
+/**
+ * @desc shift update view
+ * @route GET - /shifts/:id/update
+ * @access Private
+ * */
+
+ exports.update = asyncHandler(async (req, res, next) => {
+  return res
+    .status(200)
+    .render('pages/shifts/update', {
+      title: 'shifts',
+      development: isDev
+    });
+});
