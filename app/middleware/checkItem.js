7c31c2420973e6fc345552074c3bad76c197e8f5
Dan Moneypenny <catullus1973@gmail.com>
2022-07-22 02:40:42 -0700

    add check item middleware

--- app/middleware/checkItem.js
+++ app/middleware/checkItem.js
@@ -1,8 +1,34 @@
-const checkItem = ($model) => async (req, res, next) => {
+const checkItem = ($model, $errorResponse) => async (req, res, next) => {
 
-  console.log('check item middleware');
+  //check if item is found
+  let item = await $model.findById(req.params.id);
 
-  next();
+  try {
+    if(!item) {
+      const error = new Error('Item was not found.');
+      error.status = 404;
+      error.response = $errorResponse;
+      throw error;
+    } else {
+      res.item_result = {
+        success: true,
+        message: 'Item has been found.',
+        data: item
+      };    
+      return next();
+    }   
+  } catch(error) {
+    return next(error);
+  }
+
+  // if(!item) {
+  //   const error = new Error('Item was not found.');
+  //   error.status = 404;
+  //   error.response = 'json';
+  //   throw error;
+  // } else {
+  //   return next();
+  // }
 
 }
 
--- app/units.api/shifts/shifts.routes.js
+++ app/units.api/shifts/shifts.routes.js
@@ -11,7 +11,7 @@ const { handleQuery } = require('../../middleware');
 
 // routes
 shiftRouter.route('/').get(handleQuery(Shift), read).post(validationRules('createShift'), validate, create);
-shiftRouter.route('/:id').get(checkItem(Shift), detail).put(update).delete(remove);
+shiftRouter.route('/:id').get(checkItem(Shift, 'json'), detail).put(update).delete(remove);
 
 // export
 module.exports = shiftRouter;
